ec279fef63d3a2452dc3ad99a89671d3be163da0
Anton Orlov <i-am@bk.ru>
2015-03-07 01:33:16 +0300

    Fix quotes in url
    Change double quotes to single and use sprintf instead echo

--- app/view/NewUserView.php
+++ app/view/NewUserView.php
@@ -12,12 +12,13 @@ use Controllers;
 
 class NewUserView {
     public function giveIl($data){
-
-        return '<li class="pane1">
-                    <div class="img" style="background: url("'.$data['pic'].'") no-repeat scroll center; background-size: cover;"></div>
-                    <div>'.$data['name'].'</div>
+        $html = '<li class="pane1">
+                    <div class="img" style="background: url(\'%1$s\') no-repeat scroll center; background-size: cover;"></div>
+                    <div>%2$s</div>
                     <div class="like"></div>
                     <div class="dislike"></div>
                 </li>';
+
+        return sprintf($html, $data['pic'], $data['name']);
     }
 } 
